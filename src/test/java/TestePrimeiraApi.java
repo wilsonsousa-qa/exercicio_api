e80386d032c3e19135182fbc817117e7ff6ddef8
Wilson Sousa <wilsonsousa.qa@gmail.com>
2025-11-01 11:35:08 -0300

    Exercicio02 - Primeira Api

--- src/test/java/TestePrimeiraApi.java
+++ src/test/java/TestePrimeiraApi.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class TestePrimeiraApi {
 
     @Test
-    public void testeComSucessoPrimeiraApi(){
+    public void exercicio01_TesteComSucessoPrimeiraApi(){
 
         String url= "http://localhost:8080/api/primeiraApi";
 
@@ -18,6 +18,25 @@ public class TestePrimeiraApi {
                                 .assertThat()
                                 .statusCode(200)
                                 .body(Matchers.containsString("Retorno da api com sucesso!!!"));
+
+
+    }
+    @Test
+    public void exercicio02_TesteQueryParamPrimeiraApi(){
+
+        String url= "http://localhost:8080/api/primeiraApiV1";
+        String textoQueryParam = "Teste Qa";
+
+        RestAssured.given()
+                .queryParams("palavra",textoQueryParam)
+                .log().all()
+                .when()
+                .get(url)
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .body(Matchers.containsString(textoQueryParam));
 
 
     }
