c340d2ad27cc38f7c04acd6541327d23f8463e61
Wilson Sousa <wilsonsousa.qa@gmail.com>
2025-11-02 22:34:32 -0300

    Exercicio05 - TestExercicios

--- src/test/java/TestExercicios.java
+++ src/test/java/TestExercicios.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class TestExercicios {
 
     @Test
-    public void exercicio01_TesteParOuImpar(){
+    public void exercicio04_TesteParOuImpar(){
 
         String url= "http://localhost:8080/exercicios/parOuImpar";
         String textoQueryParam = "3";
@@ -20,6 +20,24 @@ public class TestExercicios {
                 .assertThat()
                 .statusCode(200)
                 .body(Matchers.containsString(textoQueryParam));
+
+
+    }
+    @Test
+    public void exercicio05_TesteCalcularSalario(){
+
+        String pathParamSalario = "3000";
+        String url= "http://localhost:8080/exercicios/calculaSalario/"+pathParamSalario;
+
+        RestAssured.given()
+                .log().all()
+                .when()
+                .get(url)
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .body(Matchers.containsString("Salario liquido = 2050.6516"));
 
 
     }
